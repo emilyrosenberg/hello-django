840d6fc2e5165d3219dca8c19770944ceb16f8db
Emily Rosenberg <earosenberg@gmail.com>
2023-12-27 00:13:52 +0100

    Add functionality to delete anitem (not working yet)

--- todo/views.py
+++ todo/views.py
@@ -17,7 +17,7 @@ def add_item(request):
         form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('get_todo_list')
+            return redirect('get_todo_list')
     form = ItemForm()
     context = {
         'form': form
@@ -36,3 +36,14 @@ def edit_item(request, item_id):
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
