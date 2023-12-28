72a6aa1ecf652289513c01f3ccfe478807786874
Emily Rosenberg <earosenberg@gmail.com>
2023-12-28 17:00:32 +0100

    Add settings for deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+    import env
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +82,10 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.parse('postgres://acfrvkib:aid10wZD8CkBAQcreQr1J4QHpuMBExFo@dumbo.db.elephantsql.com/acfrvkib')
+    
     }
 }
 
